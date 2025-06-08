89ba5a2d5443794eb7583fe8fa3dc901c5c26cf2
SAAD EL AHRACH <saadjone00@gmail.com>
2025-06-08 16:25:45 +0000

    make the file leaf.png as the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
       <head>
         <title>Surplus Saver</title>
         <meta name="description" content="Reduce food waste. Save money. Support local businesses." />
+        <link rel="icon" href="/leaf.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
